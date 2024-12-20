9585efa7d5e8bb4ce944cadf9c589eb98a41167a
unknown <shamsatia96@gmail.com>
2024-12-20 21:32:09 +0200

    Update

--- ScannerProject/obj/Debug/net8.0/ScannerProject.AssemblyInfo.cs
+++ ScannerProject/obj/Debug/net8.0/ScannerProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScannerProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4050ac964690312e7628d2049305a5d8a17ce954")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScannerProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScannerProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
